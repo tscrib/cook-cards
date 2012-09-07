23c40dc2bcad50f9ede52d97bc7d41ab0d649d2e
Tim Scribner <tim.scribner@gmail.com>
2012-09-07 12:06:07 -0400

    Update site layout to new template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
 			@user = User.new(params[:user])
 			if @user.save
 				sign_in @user
-				flash[:success] = "Welcome to the Sample App!"
+				flash[:success] = "Welcome to Cook Cards!"
 				redirect_to @user
 			else
 				render 'new'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,7 +19,7 @@ describe "Static pages" do
   describe "Home page" do
     before{ visit root_path }
 
-    let( :heading ) { 'Sample App' }
+    let( :heading ) { 'Cook Cards' }
     let( :page_title ) { '' }
 
     it_should_behave_like "all static pages"
@@ -89,7 +89,7 @@ describe "Static pages" do
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign Up')
-    click_link "sample app"
+    click_link "Cook Cards"
     page.should have_selector 'title', text: full_title('')
   end
 end
