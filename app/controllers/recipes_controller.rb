00c6640faec8de6d8ae1944aa99f8a45abf92e14
Tim Scribner <tim.scribner@gmail.com>
2012-09-09 12:07:14 -0400

    Implement edit and destroy functions for recipes. Tied these actions to buttons. Refactored button code to be a helper method.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
 	before_filter :signed_in_user, only: [:new, :edit, :update, :destroy]
-
+	before_filter :admin_user, only: [:edit, :update, :destroy]
 
 	def index
 		if signed_in?
@@ -10,13 +10,14 @@ class RecipesController < ApplicationController
 	end
 
 	def new
-		
+		@recipe = Recipe.new
 	end
 
 	def create
-		@recipe = User.new(params[:recipe])
-		if @user.save
-	      # Handle a successful save.
+		@recipe = Recipe.new(params[:recipe])
+		if @recipe.save
+			flash[:success] = "Your Recipe was Created!"
+			redirect_to recipe_path(@recipe)
 	    else
 	      render 'new'
 	    end
@@ -30,14 +31,39 @@ class RecipesController < ApplicationController
 	end
 
 	def edit
-		
+		# Before filters takes care of knowing who to allow
+		@recipe = Recipe.find(params[:id])
 	end
 
 	def update
-		
+		# Get local instance
+		@recipe = Recipe.find(params[:id])
+
+		# Update object with entered values
+		if @recipe.update_attributes(params[:recipe])
+			flash[:success] = "Recipe updated"
+			redirect_to @recipe
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
-		
+		# Before filters takes care of knowing who to allow
+		Recipe.find(params[:id]).destroy
+		flash[:success] = "Recipe deleted."
+		redirect_to recipes_url
+	end
+
+	# private methods
+	private
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
+
+	def admin_user
+		redirect_to(root_path) unless current_user.admin?
 	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,32 @@
 module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
-  	def full_title(page_title)
+ def full_title(page_title)
 
-    	base_title = "Cook Cards"
+   base_title = "Cook Cards"
 
-    	if page_title.empty?
+   if page_title.empty?
       		base_title			# Implicit return
-    	else
+       else
       		"#{base_title} | #{page_title}"		# String interpolation
-    	end
-  	end
-end
+       end
+     end
+
+     # 
+     # params expected:
+     # :obj
+     # :method
+     def btn_icon( icon, param={}  )
+      if param.empty?
+        button_tag nil, class: "btn btn-mini" do
+          content_tag(:i, nil, class: icon)
+        end
+      else
+
+        link_to param[:obj], class: "btn btn-mini", method: param[:method] do
+          content_tag(:i, nil, class: icon)
+        end
+      end
+      
+    end
+  end
