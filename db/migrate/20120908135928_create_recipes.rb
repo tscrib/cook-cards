13171745f29dad5509b802f2e6e81dd747cd5d4b
Tim Scribner <tim.scribner@gmail.com>
2012-09-09 01:12:22 -0400

    Fix db bug

--- db/migrate/20120908135928_create_recipes.rb
+++ db/migrate/20120908135928_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      t.integrer :user_id
+      t.integer :user_id
       t.string :title
       t.text :ingredients
       t.text :directions
